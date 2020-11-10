7da65bcb4f5a688ce67be8807b466e5c282acde9
Luis Carranza <luiscarranza3395@gmail.com>
2020-11-09 19:52:19 -0500

    Getters and Setters de las clases

--- src/main/java/com/redsocial/entity/Director.java
+++ src/main/java/com/redsocial/entity/Director.java
@@ -30,6 +30,38 @@ public class Director {
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "idGrado")
 	private Grado grado;
+
+	public int getIdDirector() {
+		return idDirector;
+	}
+
+	public void setIdDirector(int idDirector) {
+		this.idDirector = idDirector;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public Grado getGrado() {
+		return grado;
+	}
+
+	public void setGrado(Grado grado) {
+		this.grado = grado;
+	}
 	
 	
 	
